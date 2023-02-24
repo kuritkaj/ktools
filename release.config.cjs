c0814981afb484e3701b27cfac66ae61ee661745
Jakub Kuřitka <jakub.kuritka@gmail.com>
2023-02-25 00:01:53 +0100

    chore: trigger release also for refactor type commits

--- release.config.cjs
+++ release.config.cjs
@@ -1,40 +1,44 @@
 module.exports = {
-    branches: [
-        {
-            name: 'main',
-        }
+  branches: [
+    {
+      name: 'main',
+    }
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          {
+            type: 'chore',
+            release: 'minor',
+          },
+          {
+            type: 'refactor',
+            release: 'minor',
+          },
+          {
+            type: '*!',
+            release: 'major',
+          },
+        ],
+      },
     ],
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                preset: 'conventionalcommits',
-                releaseRules: [
-                    {
-                        type: 'chore',
-                        release: 'minor',
-                    },
-                    {
-                        type: '*!',
-                        release: 'major',
-                    },
-                ],
-            },
-        ],
-        '@semantic-release/release-notes-generator',
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: 'CHANGELOG.md',
-            },
-        ],
-        '@semantic-release/npm',
-        [
-            '@semantic-release/git',
-            {
-                assets: ['package.json',  'package-lock.json', 'CHANGELOG.md', "README.md"],
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-            },
-        ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
     ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md', "README.md"],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
 };
